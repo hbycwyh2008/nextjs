c2666968a8e3d55b0bc1650716e0091d18bf92b2
Morgan <ycshinichi@163.com>
2026-03-04 14:46:11 +0800

    css styling and css moudule

---
+++ CSS Sytling & CSS Module/src/App.js
@@ -0,0 +1,11 @@
+import Post from "./components/Post";
+function App() {
+  return <main>
+              <Post author="John" body="This is John's post." />
+              <Post author="Jane" body="This is Jane's post." />
+
+    </main>;
+  
+}
+
+export default App;
